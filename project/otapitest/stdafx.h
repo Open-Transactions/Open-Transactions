a18278d0d13bb2cf2534cf9557e6046bf713f40e
BugFreeSoftware <github@bugfree.be>
2014-03-10 21:46:07 -0700

    Fixed otapitest make file

--- project/otapitest/stdafx.h
+++ project/otapitest/stdafx.h
@@ -8,8 +8,9 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #include <stdio.h>
+#ifdef _WIN32
 #include <tchar.h>
-
+#endif
 
 
 // TODO: reference additional headers your program requires here
