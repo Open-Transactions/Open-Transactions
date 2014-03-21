d00743ec155d6a7bffaa148666c7824cfd1588a6
BugFreeSoftware <github@bugfree.be>
2014-03-21 20:41:23 +0100

    Removed unit test related stuff
    (rebased by da2ce7)

--- src/otapitest/Test_create_pseudonym.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.hpp"
-
-
-TEST_MULTI(create_pseudonym)
-{
-	EXPECT_MULTI_RETURN("", NYM_ID, CreateNym(2048, NYM_SOURCE, ""));
-
-	ASSERT_MULTI_EQ("", NYM_ID, me.create_pseudonym(2048, NYM_SOURCE, ""));
-}
