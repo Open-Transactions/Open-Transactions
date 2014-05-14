0a226941324f587073e43daee44cbb411908dd56
Cameron Garnham <da2ce7@gmail.com>
2014-05-14 23:09:14 +1000

    bump version number to 0.92.c

--- include/otlib/constants.h
+++ include/otlib/constants.h
@@ -15,11 +15,11 @@
 
 // Global
 #ifndef OT_VERSION
-#define OT_VERSION "0.92.b"
+#define OT_VERSION "0.92.c"
 #endif
 
 #ifndef OT_PRODUCTVERSION
-#define OT_PRODUCTVERSION 0,92,2,0
+#define OT_PRODUCTVERSION 0,92,3,0
 #endif
 
 #ifndef OT_API_NAME
