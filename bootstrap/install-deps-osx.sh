8bf7d5b19f5f77b31be2553fcd365a21b875396c
Tommy Back <tommy.jh.back@gmail.com>
2014-05-20 20:11:16 +0200

    brew fixes and removed zmq.

--- bootstrap/install-deps-osx.sh
+++ bootstrap/install-deps-osx.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-brew install protobuf-c protobuf zeromq32 boost
+brew update
+brew install protobuf-c protobuf boost
 
 git clone https://github.com/zeromq/cppzmq.git
 sudo cp cppzmq/zmq.hpp /usr/local/include/
