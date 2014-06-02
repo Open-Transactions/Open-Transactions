199eb6f1b5367fd45bb4bbd4fa69ed97ec9a3b67
Filip Gospodinov <filip@monetas.net>
2014-06-02 15:15:24 +0200

    travis: merge Linux and OS X install scripts
    This way we can provide scalability for more OS variations and
    avoid code duplication.

--- scripts/travis/install-deps-osx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-brew update
-brew install protobuf-c protobuf boost openssl
-brew link --force openssl
-
-git clone https://github.com/zeromq/cppzmq.git
-sudo cp cppzmq/zmq.hpp /usr/local/include/
